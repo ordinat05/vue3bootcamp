f25b29ded0986049955b97a029c86d9d142e4c73
sebahattin <sebkucuk05@gmail.com>
2022-09-19 17:24:50 +0300

    d4commit1

---
+++ ders4/0-component/app.js
@@ -0,0 +1,33 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      counter2: 0,
+      itemList: [],
+    };
+  },
+  methods: {
+    addItem() {
+      this.itemList.push(new Date().getTime());
+    },
+  },
+  computed: {},
+  watch: {},
+});
+app.component("counter-item", {
+  data() {
+    return {
+      counter: 0,
+      counter2: 0,
+    };
+  },
+  // String Template Component
+  template: `
+  <div class="container-sm">
+    <h3 class="mb-2"> {{ counter }}</h3>
+    <button @click="counter--" class="red sm ">-</button>
+    <button @click="counter++" class="green sm sebom_mr2">+</button>
+</div>`,
+});
+
+app.mount("#app");
