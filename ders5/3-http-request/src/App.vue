dcb92a7f350f48dc5e91815a66fed6b83619cac0
sebahattin <sebkucuk05@gmail.com>
2022-09-29 05:04:25 +0300

    d5son1

--- ders5/3-http-request/src/App.vue
+++ ders5/3-http-request/src/App.vue
@@ -5,16 +5,17 @@
     <div class="my-2">
       <input type="text" placeholder="ne alacaksın?" @keydown.enter="onSave" />
     </div>
-    <ul>
+    <ul v-if="itemsList.length > 0">
       <li
         v-for="item in itemsList"
         :key="item.id"
         class="d-flex justify-content-between align-items-center"
       >
         <span>{{ item.title }}</span>
-        <button class="sm red">Sil</button>
+        <button @click="onDelete(item)" class="sm red">Sil</button>
       </li>
     </ul>
+    <div v-else class="bg-blue text-white">Herhangibir Ürün yoktur</div>
     <small class="mt-2 text-red d-flex justify-content-end align-items-center"
       >{{ itemsList.length }} {{ itemCount }} adet alınacak ürün var.</small
     >
@@ -49,9 +50,18 @@ export default {
         .post("http://localhost:3000/items", saveObject)
         .then((save_response) => {
           console.log(save_response);
+          this.itemsList.push(save_response.data);
+          e.target.value = "";
+          e.target.focus();
         });
-      e.target.value = "";
-      e.target.focus();
+    },
+    onDelete(item) {
+      axios
+        .delete(`http://localhost:3000/items/${item.id}`)
+        .then((delete_response) => {
+          console.log(delete_response);
+          this.itemsList = this.itemsList.filter((i) => i.id != item.id);
+        });
     },
   },
   // components: {},
